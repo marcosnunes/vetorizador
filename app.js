37b215a96df0a0eb351f718936213e858cdd97b8
marcosnunes <marcos.nunes.lph@outlook.com>
2025-11-20 00:36:29 -0300

    Update

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ try {
 }
 
 // --- MAPA ---
-const map = L.map('map').setView([-25.567859, -49.359602], 30);
+const map = L.map('map').setView([-25.567859, -49.359602], 10);
 
 const satelliteMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
   attribution: 'Tiles &copy; Esri',
