a3858db4a45a111daaafafe698c594832267322b
marcosnunes <marcos.nunes.lph@outlook.com>
2025-11-20 00:27:28 -0300

    Update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import init, { vetorizar_imagem } from './pkg/vetoriza.js'; // CORRIGIDO
+import init, { vetorizar_imagem } from './pkg/vetoriza.js';
 
 // --- CONFIGURAÇÃO INICIAL ---
 const loader = document.getElementById('loader-overlay');
